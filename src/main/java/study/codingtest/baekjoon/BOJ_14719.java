8dc59c38ba3c6aabd04ee3b7bc39ff798cacd539
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-09-08 01:06:49 +0900

    feat: 빗물 BOJ_14719.java
    two pointer

---
+++ src/main/java/study/codingtest/baekjoon/BOJ_14719.java
@@ -0,0 +1,36 @@
+package study.codingtest.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_14719 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String ignore = br.readLine();
+    int[] height = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+
+    int volume = 0;
+    int left = 0;
+    int right = height.length - 1;
+    int leftMax = height[left];
+    int rightMax = height[right];
+
+    while (left < right) {
+      leftMax = Integer.max(height[left], leftMax);
+      rightMax = Integer.max(height[right], rightMax);
+
+      if (leftMax <= rightMax) {
+        volume += leftMax - height[left];
+        left++;
+      } else {
+        volume += rightMax - height[right];
+        right--;
+      }
+    }
+    System.out.println(volume);
+  }
+
+}
