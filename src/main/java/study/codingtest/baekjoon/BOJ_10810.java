6520ea4acdd9938caab0730b5cd3bc1dacf9b503
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-09-15 00:36:25 +0900

    feat: 8개 추가

---
+++ src/main/java/study/codingtest/baekjoon/BOJ_10810.java
@@ -0,0 +1,31 @@
+package study.codingtest.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_10810 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int[] arr = new int[n];
+    for (int i = 0; i < m; i++) {
+      StringTokenizer round = new StringTokenizer(br.readLine());
+      int start = Integer.parseInt(round.nextToken()) - 1;
+      int end = Integer.parseInt(round.nextToken());
+      int ball = Integer.parseInt(round.nextToken());
+      Arrays.fill(arr, start, end, ball);
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int j : arr) {
+      sb.append(j).append(" ");
+    }
+    System.out.println(sb);
+  }
+
+}
