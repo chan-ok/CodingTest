22601874d0cee8639326c6dedfb6cd12f7254f45
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-10-18 21:44:05 +0900

    feat: IOIOI BOJ_5525.java

---
+++ src/main/java/study/codingtest/baekjoon/string/BOJ_5525.java
@@ -0,0 +1,67 @@
+package study.codingtest.baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_5525 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String pattern = "IO".repeat(Integer.parseInt(br.readLine())).concat("I");
+    br.readLine();
+    String source = br.readLine();
+    System.out.println(searchByKMP(source, pattern));
+  }
+
+  public static int searchByKMP(String source, String pattern) {
+    int patternLen = pattern.length();
+    int sourceLen = source.length();
+
+    int[] pi = computePiArray(pattern);
+    int pp = 0;
+    int sp = 0;
+    int count = 0;
+    while ((sourceLen - sp) >= (patternLen - pp)) {
+      if (pattern.charAt(pp) == source.charAt(sp)) {
+        pp++;
+        sp++;
+      }
+      if (pp == patternLen) {
+        count++;
+        pp = pi[pp - 1];
+      } else if (sp < sourceLen && pattern.charAt(pp) != source.charAt(sp)) {
+        if (pp != 0) {
+          pp = pi[pp - 1];
+        } else {
+          sp++;
+        }
+      }
+    }
+
+    return count;
+  }
+
+  public static int[] computePiArray(String pattern) {
+    int len = 0;
+    int i = 1;
+    int[] pi = new int[pattern.length()];
+
+    while (i < pattern.length()) {
+      if (pattern.charAt(i) == pattern.charAt(len)) {
+        len++;
+        pi[i] = len;
+        i++;
+      } else {
+        if (len != 0) {
+          len = pi[len - 1];
+        } else {
+          pi[i] = len;
+          i++;
+        }
+      }
+    }
+
+    return pi;
+  }
+}
