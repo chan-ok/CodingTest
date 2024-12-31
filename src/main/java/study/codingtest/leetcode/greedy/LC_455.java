5934510b2b3ca39002834125a23750ada02efa9a
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-12-31 18:03:12 +0900

    feat: 쿠키 부여 LC_455

---
+++ src/main/java/study/codingtest/leetcode/greedy/LC_455.java
@@ -0,0 +1,21 @@
+package study.codingtest.leetcode.greedy;
+
+import java.util.Arrays;
+
+public class LC_455 {
+  public int findContentChildren(int[] g, int[] s) {
+    Arrays.sort(g);
+    Arrays.sort(s);
+    
+    int i = 0;
+    int j = 0;
+
+    while (i < g.length && j < s.length) {
+      if (s[j] >= g[i]) {
+        i++;
+      }
+      j++;
+    }
+    return i;
+  }
+}
